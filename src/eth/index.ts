e6d2713e4befccfebaf11b7e9a23cdf6cbe4a48b
Dmitriy Gaydukov <gaydukov89@gmail.com>
2019-04-26 18:12:09 +0800

    add eth hd wallet support

--- src/eth/index.ts
+++ src/eth/index.ts
@@ -2,18 +2,65 @@
  * Ethereum Cold Storage
  * Basically we neeed only one library ethereumjs-util. That would be enough for everything
  * If we want to generate keys, we can use randomBytes from crypto library
+ * To work with tx building we need ethereumjs-tx library
+ * 
+ * https://github.com/ethereumjs/ethereumjs-util
+ * https://github.com/ethereumjs/ethereumjs-tx
+ * https://github.com/cryptocoinjs/secp256k1-node
  */
 
 const { randomBytes } = require('crypto');
 const util = require('ethereumjs-util');
 const EthereumTx = require('ethereumjs-tx')
+const bip39 = require('bip39');
+const HDKey = require('hdkey')
 import {ICryptoStorage} from '../app/interfaces';
  
 
  export default class EthStorage implements ICryptoStorage{
 
+   constructor(){
+      const wallet = this.generateHdWallet();
+      const keyPair = this.deriveKeyPair(123, wallet.masterPrivateKey);
+      console.log(keyPair)
+   }
+
+   generateHdWallet(){
+      const mnemonic = bip39.generateMnemonic();
+      const seed = bip39.mnemonicToSeedSync(mnemonic);
+      const hdkey = HDKey.fromMasterSeed(seed);
+      return {
+         masterPrivateKey: hdkey.privateExtendedKey,
+         masterPublicKey: hdkey.publicExtendedKey
+      }
+   }
+
+   deriveKeyPair(index, masterPrivateKey){
+      const hdkey = HDKey.fromExtendedKey(masterPrivateKey);
+      const path = `m/44'/60'/0'/0/${index}`;
+      const child = hdkey.derive(path);
+      const privateKey = child.privateKey;
+      const publicKey = util.privateToPublic(child.privateKey);
+      const address = util.pubToAddress(publicKey);
+      const keyPair = {
+         privateKey: privateKey.toString('hex'),
+         publicKey: publicKey.toString('hex'),
+         address: '0x' + address.toString('hex'),
+      };
+      return keyPair;
+   }
+
    generateKeyPair(){
-      const privateKey = randomBytes(32);
+      /**
+       * It is adviced to check whether we generate correct private key or not here https://github.com/cryptocoinjs/secp256k1-node
+       * But from my personal experience, I can say, that randomBytes always generate right key, moreover, privatekey - just a number
+       * it can't be incorrect. But just in case...
+       */
+      let privateKey;
+      do {
+         privateKey = randomBytes(32)
+      } while (!util.isValidPrivate(privateKey));
+
       const publicKey = util.privateToPublic(privateKey);
       const address = util.pubToAddress(publicKey);
       const keyPair = {
@@ -21,6 +68,7 @@ import {ICryptoStorage} from '../app/interfaces';
          publicKey: publicKey.toString('hex'),
          address: '0x' + address.toString('hex'),
       }
+
       return keyPair;
    }
 
