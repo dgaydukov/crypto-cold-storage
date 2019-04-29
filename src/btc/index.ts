60fce60c835370b130d1cdcb14aec7abd848789a
Dmitriy Gaydukov <gaydukov89@gmail.com>
2019-04-29 14:17:10 +0800

    add btc sign/verify

--- src/btc/index.ts
+++ src/btc/index.ts
@@ -6,20 +6,20 @@ import { ECPair, payments, TransactionBuilder, address as BitcoinAddress } from 
 const bip39 = require('bip39');
 const HDKey = require('hdkey');
 import { ICryptoStorage } from '../app/interfaces';
-
-var bitcore = require('bitcore-lib');
-var Message = require('bitcore-message');
+const secp256k1 = require('secp256k1')
+const { createHash } = require('crypto');
 
 export default class BtcStorage implements ICryptoStorage {
 
     constructor() {
-    //   const privateKey = 'a121f2bd62a5126dcd4ee357ec783b7678b262e545342ed4986aed7c47dd3129';
-    //   const publicKey = this.getPublicKeyFromPrivateKey(privateKey);
+      const privateKey = 'a121f2bd62a5126dcd4ee357ec783b7678b262e545342ed4986aed7c47dd3129';
+      const msg = 'hello world!';
+      const publicKey = this.getPublicKeyFromPrivateKey(privateKey);
       
-    //   var privateKey = bitcore.PrivateKey.fromWIF('cPBn5A4ikZvBTQ8D7NnvHZYCAxzDZ5Z2TSGW2LkyPiLxqYaJPBW4');
-    //   var signature = Message('hello, world').sign(privateKey);
-    //   console.log(signature)
-    }
+      const sig = this.sign(msg, privateKey);
+      const check = this.verify(msg, sig, publicKey);
+      console.log(check, sig)
+    } 
 
     generateHdWallet() {
         const mnemonic = bip39.generateMnemonic();
@@ -81,15 +81,24 @@ export default class BtcStorage implements ICryptoStorage {
     }
 
     sign(msg, privateKey) {
-        return '';
+        const messageHash = this.hashMessage(msg);
+        const sigObj = secp256k1.sign(Buffer.from(messageHash, 'hex'), Buffer.from(privateKey, 'hex'));
+        const sig = sigObj.signature.toString('base64');
+        return sig;
     }
 
     verify(msg, sig, publicKey) {
-        return false;
+        const messageHash = this.hashMessage(msg);
+        return secp256k1.verify(Buffer.from(messageHash, 'hex'), Buffer.from(sig, 'base64'), Buffer.from(publicKey, 'hex'));;
     }
 
     hashMessage(msg){
-        return '';
+        return this.sha256(this.sha256(msg));
+    }
+
+
+    sha256(msg){
+        return createHash('sha256').update(msg).digest('hex');
     }
 
     recoverPublicKey(msg, sig) {
